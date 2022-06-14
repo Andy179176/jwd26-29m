ad4fffe6645580511f746d30fc0839905f83ae4f
andy179176 <andy179176@gmail.com>
2022-06-14 15:42:15 +0200

    20220609

--- 20220607_1/src/com/company/MyScanner.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class MyScanner implements Scannerable {
-    @Override
-    public String scan() {
-        System.out.println("Сканирую....");
-        return "The scanned document return ";
-    }
-}
--- 20220607_1/src/com/company/Scannerable.java
+++
@@ -1,6 +0,0 @@
-package com.company;
-
-public interface Scannerable {
-    String scan();
-
-}
