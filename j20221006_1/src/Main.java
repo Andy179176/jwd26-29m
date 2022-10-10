f318c04f8f0bdc5c408ef7de022c8fa8b7715715
andy179176 <andy179176@gmail.com>
2022-10-10 08:39:21 +0200

    20221007

--- j20221006_1/src/Main.java
+++ j20221006_1/src/Main.java
@@ -1,10 +1,13 @@
 import java.io.*;
 import java.net.URL;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            demo1IOByteByByte();
+            //demo1IOByteByByte();
+            //demo2IOArray();
+            demo3();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
@@ -15,22 +18,54 @@ public class Main {
         //InputStream fis = new ByteArrayInputStream(new byte[]{97,98,99,100});
         //InputStream fis = new URL("https://www.google.com/").openStream();
         //OutputStream fos = new FileOutputStream("google.html", true);
+        OutputStream fos = new FileOutputStream("file1.txt", true);
 
         int data;
         while (  ( data= fis.read() )!=-1 ){
             //System.out.printf("%d (%x) %c%n", data,data,data);
             System.out.print((char)data);
-            //fos.write(data);
+            fos.write(data);
         }
-        System.out.println(data);
+        //System.out.println(data);
         // 0 .. 255    //00..FF
         // -128 ... 127
 
-        //fos.close();
+
+        fos.flush();
+        fos.close();
         fis.close();
+
     }
 
+    public static void demo2IOArray() throws IOException {
+        InputStream fis = new FileInputStream("file.txt");
+//        OutputStream fos = new FileOutputStream("file1.txt", true)
 
+        byte[] data= new byte[1024*1024];
+        int count;
+        while (  ( count= fis.read(data) )!=-1 ){
+           System.err.println(count);
+           if(count<data.length){
+               System.out.println(new String(Arrays.copyOf(data,count)));
+           } else {
+               System.out.print(new String(data));
+           }
+  //          fos.write(data);
+        }
+
+
+//        fos.flush();
+//        fos.close();
+        fis.close();
+
+    }
+
+    public static void demo3() throws IOException {
+        InputStream fis = new FileInputStream("file.txt");
+        byte[] bytes = fis.readAllBytes();
+        System.out.println(new String(bytes));
+        fis.close();
+    }
 
 
 }
