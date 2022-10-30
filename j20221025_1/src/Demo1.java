1b53205806fc32172cd2e3f14982a7f49c0c604a
andy179176 <andy179176@gmail.com>
2022-10-30 11:41:42 +0100

    20221027

--- j20221025_1/src/Demo1.java
+++
@@ -1,9 +0,0 @@
-public class Demo1 extends Thread {
-    @Override
-    public void run(){
-
-        for (int i = 0; i < 150; i++) {
-            System.out.println(this.getId() + " " + this.getName() + "  " + i);
-        }
-    }
-}
--- j20221025_1/src/Demo2.java
+++
@@ -1,9 +0,0 @@
-public class Demo2 implements  Runnable{
-    @Override
-    public void run() {
-
-        for (int i = 0; i < 500; i++) {
-            System.out.println(i);
-        }
-    }
-}
