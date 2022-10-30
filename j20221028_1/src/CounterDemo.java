8a72f29e488d4c4711025ff65d342e5aacb12b97
andy179176 <andy179176@gmail.com>
2022-10-30 11:44:31 +0100

    20221028

---
+++ j20221028_1/src/CounterDemo.java
@@ -0,0 +1,63 @@
+public class CounterDemo {
+    private Object lock = new Object();
+
+    private final int LIMIT = 10_000;
+    private int counter = 0;
+
+    public static void main(String[] args) {
+
+        CounterDemo obj = new CounterDemo();
+        obj.createThreads(5);
+        System.out.println(obj);
+
+
+    }
+
+    public void createThreads(int threadCount){
+        Thread[] threads = new Thread[threadCount];
+        for (int i = 0; i<threads.length; i++){
+            threads[i]= new Thread(this::add);
+        }
+        for (Thread t: threads){
+            t.start();
+        }
+        for (Thread t: threads){
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
+    public  void add(){
+
+
+        for (int i = 0; i < LIMIT ; i++) {
+
+            synchronized (lock){
+                counter++;
+            }
+
+
+            /*addCounter();
+
+            if(counter%5000==0){
+                System.out.println(Thread.currentThread().getName());
+            }
+
+             */
+
+            // read the value of counter
+            // add 1 to counter value
+            // write new counter value
+        }
+    }
+    private synchronized void addCounter(){counter++;}
+
+    @Override
+    public String toString() {
+        return "counter=" + counter;
+    }
+}
