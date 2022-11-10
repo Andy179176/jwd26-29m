18eb4bff203764dbd5a5a748a7e7e4b16142d1e3
andy179176 <andy179176@gmail.com>
2022-11-10 22:46:58 +0100

    20221110

---
+++ j20221104_1/src/Main2.java
@@ -0,0 +1,59 @@
+public class Main2 {
+
+    public static void main(String[] args) throws InterruptedException {
+        PingPong pingPong = new PingPong();
+        Thread thread1 = new Thread(pingPong::printPing);
+        Thread thread2 = new Thread(pingPong::printPong);
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread2.join();
+    }
+
+
+}
+
+class PingPong {
+    private static String str = "pong";
+    private final Object lock = new Object();
+    private final int LIMIT = 20;
+
+    void printPong() {
+        for (int i = 0; i < LIMIT; ) {
+
+            synchronized (lock) {
+                if (str.equals("ping")) {
+                    str = "pong";
+                    i++;
+                    System.out.println(i+" "+str);
+                }
+            }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
+    void printPing() {
+        for (int i = 0; i < LIMIT; ) {
+
+            synchronized (lock) {
+                if (str.equals("pong")) {
+                    str = "ping";
+                    i++;
+                    System.out.println(i+ " "+str);
+                }
+            }
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+    }
+
+}
